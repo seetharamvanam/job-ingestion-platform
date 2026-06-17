1a21ad46c044e10816673fe031b587fe3194fc20
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2026-06-17 04:02:04 -0500

    Initial Commit

---
+++ src/main/java/com/jobingestion/jobingestionplatform/JobIngestionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jobingestion.jobingestionplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobIngestionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobIngestionPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobingestion/jobingestionplatform/JobIngestionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobingestion.jobingestionplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobIngestionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
