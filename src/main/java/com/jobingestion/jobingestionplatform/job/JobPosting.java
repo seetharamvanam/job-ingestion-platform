a08e9330f4e0edd2d560e3dd44f7d8029e337f22
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2026-06-18 19:04:28 -0500

    creating entities, docker setup for database connection and database setup

---
+++ src/main/java/com/jobingestion/jobingestionplatform/job/JobPosting.java
@@ -0,0 +1,72 @@
+package com.jobingestion.jobingestionplatform.job;
+
+import com.jobingestion.jobingestionplatform.source.JobSource;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(
+        name = "job_postings",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {
+                        "job_source_id", "external_job_id"
+                })
+})
+public class JobPosting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "external_job_id", nullable = false)
+    private Long externalJobId;
+
+
+    @ManyToOne(fetch =  FetchType.LAZY, optional = false)
+    @JoinColumn(name = "job_source_id", nullable = false)
+    private JobSource jobSource;
+
+    @Column
+    private String title;
+
+    @Column
+    private String department;
+
+    @Column
+    private String location;
+
+    @Column
+    private String jobUrl;
+
+    @Column
+    private LocalDateTime discoveredAt;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+
+        if (this.discoveredAt == null) {
+            this.discoveredAt = LocalDateTime.now();
+        }
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/jobingestion/jobingestionplatform/source/JobSource.java
@@ -0,0 +1,47 @@
+package com.jobingestion.jobingestionplatform.source;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "job_sources")
+public class JobSource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String companyName;
+
+    @Column(columnDefinition = "text", nullable = false)
+    private String careerUrl;
+
+    @Column(nullable = false)
+    private Boolean activeStatus;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
